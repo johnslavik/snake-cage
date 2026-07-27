8d6d67e9c76dc38570e5ea20c3f36042e2a4d4eb
Bartosz Sławecki <bartosz@ilikepython.com>
2026-07-27 11:14:02 +0200

    Run this stuff for me

--- stuff.py
+++ stuff.py
@@ -1,4 +1,19 @@
-import asyncio
+from asyncio.events import _Local
+from traceback import print_tb
+from warnings import warn
+
+def _Local__del__(self, _warn=warnings.warn, _ptb=print_tb):
+    loop = self._loop
+    if loop and not loop.is_closed():
+        _warn(f"unclosed event loop {loop!r}", ResourceWarning, source=loop)
+        if not self.is_running():
+            try:
+                self.close()
+            except Exception as e:
+                _ptb(exc.__traceback__)
+
+_Local.__del__ = _Local__del__
+
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 loop.run_until_complete(asyncio.sleep(1))
